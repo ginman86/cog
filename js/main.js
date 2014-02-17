c04cdca3c78d6db677323363671a9151e5005de9
Greg Inman <ginman86@gmail.com>
2014-02-16 20:03:58 -0600

    Protodata
    Added in some fake(proto) data.

--- js/main.js
+++ js/main.js
@@ -2,9 +2,32 @@ $(document).ready
 (function()
 {
 	//something
-	var login 	= $("#login");
-		main	= $("#main");
+	var login 	= $("#login"),
+		main	= $("#main"),
+		data	= 
+		{
+			block1:
+			[
+				{english: "war", german:"krieg"}, 
+				{english: "hello", german:"gutentag!"}, 
+				{english: "percent", german:"prozent"}
+			],
+			block2:
+			[
+				1,2,3
+			]
+		};
 
+
+	$.each
+	(data.block1,
+		function(i, item)
+		{
+			console.log(item.english);
+			console.log(item.german);
+		}
+	);
+	console.log()
 	$("#idInput").on
 	("keyup", function(e)
 	{
