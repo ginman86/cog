f9c14c4480e5d426350d6d6554ed2ef41b409bc4
Greg Inman <ginman86@gmail.com>
2014-02-18 19:50:35 -0600

    Proof of concept data binding
    Added some code to show how data binding could be achieved.

--- js/main.js
+++ js/main.js
@@ -4,88 +4,9 @@ $(document).ready
 	//something
 	var login 	= $("#login"),
 		main	= $("#main"),
-		data	= 
-		{
-			block1:
-			[
-				{english: "time", german:"zeit"}, 
-				{english: "people", german:"leute"}, 
-				{english: "now", german:"jetzt"},
-				{english: "year", german:"jahr"}, 
-				{english: "work", german:"arbeit"}, 
-				{english: "free", german:"frei"},
-				{english: "here", german:"hier"}, 
-				{english: "back", german:"zurueck"},
-				{english: "company", german:"firma"}, 
-				{english: "contact", german:"kontakt"},
-				{english: "development", german:"bildung"}, 
-				{english: "full", german:"voll"},
-				{english: "service", german:"dienst"}, 
-				{english: "date", german:"datum"},
-				{english: "information", german:"auskunft"}, 
-				{english: "world", german:"welt"},
-				{english: "news", german:"zeitung"}, 
-				{english: "group", german:"gruppe"},
-				{english: "start", german:"anfang"}, 
-				{english: "support", german:"hilfe"}
-			],
-			block2:
-			[
-				{english: "", german:""}, 
-				{english: "", german:""}, 
-				{english: "", german:""},
-				{english: "", german:""}, 
-				{english: "", german:""}, 
-				{english: "", german:""},
-				{english: "", german:""}, 
-				{english: "", german:""},
-				{english: "", german:""}, 
-				{english: "", german:""},
-				{english: "", german:""}, 
-				{english: "", german:""},
-				{english: "", german:""}, 
-				{english: "", german:""},
-				{english: "", german:""}, 
-				{english: "", german:""},
-				{english: "", german:""}, 
-				{english: "", german:""},
-				{english: "", german:""}, 
-				{english: "", german:""}
-			],
-			block3:
-			[
-				{english: "", german:""}, 
-				{english: "", german:""}, 
-				{english: "", german:""},
-				{english: "", german:""}, 
-				{english: "", german:""}, 
-				{english: "", german:""},
-				{english: "", german:""}, 
-				{english: "", german:""},
-				{english: "", german:""}, 
-				{english: "", german:""},
-				{english: "", german:""}, 
-				{english: "", german:""},
-				{english: "", german:""}, 
-				{english: "", german:""},
-				{english: "", german:""}, 
-				{english: "", german:""},
-				{english: "", german:""}, 
-				{english: "", german:""},
-				{english: "", german:""}, 
-				{english: "", german:""}
-			]
-		};
+		data	= private.getData(),
+		mainScope;		
 
-
-	$.each
-	(data.block1,
-		function(i, item)
-		{
-			console.log(item.english);
-			console.log(item.german);
-		}
-	);
 	console.log()
 	$("#idInput").on
 	("keyup", function(e)
@@ -96,6 +17,16 @@ $(document).ready
 			{
 				login.hide()
 				main.show();
+
+				$.each
+				(data.block1,
+					function(i, item)
+					{
+						console.log(item.english);
+						console.log(item.german);
+						mainScope.bindPhrase(item);
+					}
+				);
 			}
 			else
 			{
@@ -103,8 +34,99 @@ $(document).ready
 			}
 
 		}
-	}
-	)
-}
+	});
 
-)
+	mainScope =
+	{
+		knownPhrase: 	$('.knownPhrase', $('#main')),
+		unknownPhrase: 	$('.unknownPhrase', $('#main')),
+		bindPhrase:
+		function(phrase)
+		{
+			if (phrase)
+			{
+				mainScope.knownPhrase.append('<br/>'+ phrase.english);
+				mainScope.unknownPhrase.append('<br/>'+ phrase.german);				
+			}
+		}
+	};
+});
+
+	var private =
+	{
+		getData:
+		function()
+		{
+			return {	block1:						
+						[
+							{english: "time", german:"zeit"}, 
+							{english: "people", german:"leute"}, 
+							{english: "now", german:"jetzt"},
+							{english: "year", german:"jahr"}, 
+							{english: "work", german:"arbeit"}, 
+							{english: "free", german:"frei"},
+							{english: "here", german:"hier"}, 
+							{english: "back", german:"zurueck"},
+							{english: "company", german:"firma"}, 
+							{english: "contact", german:"kontakt"},
+							{english: "development", german:"bildung"}, 
+							{english: "full", german:"voll"},
+							{english: "service", german:"dienst"}, 
+							{english: "date", german:"datum"},
+							{english: "information", german:"auskunft"}, 
+							{english: "world", german:"welt"},
+							{english: "news", german:"zeitung"}, 
+							{english: "group", german:"gruppe"},
+							{english: "start", german:"anfang"}, 
+							{english: "support", german:"hilfe"}
+						],
+						block2:
+						[
+							{english: "", german:""}, 
+							{english: "", german:""}, 
+							{english: "", german:""},
+							{english: "", german:""}, 
+							{english: "", german:""}, 
+							{english: "", german:""},
+							{english: "", german:""}, 
+							{english: "", german:""},
+							{english: "", german:""}, 
+							{english: "", german:""},
+							{english: "", german:""}, 
+							{english: "", german:""},
+							{english: "", german:""}, 
+							{english: "", german:""},
+							{english: "", german:""}, 
+							{english: "", german:""},
+							{english: "", german:""}, 
+							{english: "", german:""},
+							{english: "", german:""}, 
+							{english: "", german:""}
+						],
+						block3:
+						[
+							{english: "", german:""}, 
+							{english: "", german:""}, 
+							{english: "", german:""},
+							{english: "", german:""}, 
+							{english: "", german:""}, 
+							{english: "", german:""},
+							{english: "", german:""}, 
+							{english: "", german:""},
+							{english: "", german:""}, 
+							{english: "", german:""},
+							{english: "", german:""}, 
+							{english: "", german:""},
+							{english: "", german:""}, 
+							{english: "", german:""},
+							{english: "", german:""}, 
+							{english: "", german:""},
+							{english: "", german:""}, 
+							{english: "", german:""},
+							{english: "", german:""}, 
+							{english: "", german:""}
+						]
+					};					
+			}
+		};
+				
